a67bd7b5c96e6f5780a425c7caebdab9d5be1321
Arihant Jain <81769948+arihantjain916@users.noreply.github.com>
2021-07-25 17:41:53 +0530

    Add files via upload

--- src/find_the_index_of_occurence.java
+++ src/find_the_index_of_occurence.java
@@ -1,37 +1,46 @@
 public class find_the_index_of_occurence {
-    public static void main(String[] args) {
-        int a[] = { 1, 2, 3, 4, 5, 4, 8 };
-        int i = 0;
-        int d = 4;
-        int e = 0;
-        int index = index_of_occ(a, i, d , e);
-        System.out.println("Index of occurence is " + index);
-    }
+    // public static void main(String[] args) {
+    //     int a[] = { 1, 2, 3, 4, 5, 4, 8 };
+    //     int i = 0;
+    //     int d = 4;
+    //     int index = index_of_occ(a, i, d);
+    //     System.out.println("Index of occurence is " + index);
+    // }
 
-    public static int index_of_occ(int[] a, int index, int d, int e) {
-        if (a.length == index) {
-            return -1;
-        }
-        if (index == a[index]) {
-            index_of_occ(a, index + 1, d , e+1);
-            return index;
-        } else {
-            return 0;
-        }
-    }
+    // to print index from starting
+
+    // public static int index_of_occ(int[] a , int index , int d) {
+    // if(a.length == index){
+    // return -1;
+    // }
+    // if(d == a[index]){
+    // return index;
+    // }
+    // else{
+    // int b = index_of_occ(a, index + 1 , d);
+    // return b;
+    // }
+    // }
+
+    // To print index from last
+    // public static int index_of_occ(int[] a, int index, int d) {
+    //     if (a.length == index) {
+    //         return -1;
+    //     }
+    //     int b = index_of_occ(a, index + 1, d);
+    //     if(b == -1){
+    //         if (d == a[index]) {
+    //             return index;
+    //         } else {
+    //             return -1;
+    //         }
+    //     }
+    //     else{
+    //         return b;
+    //     }
+    // }
+    
+    // to print all the occurence of the given value in array
+    
+
 }
-// public static int index_of_occ(int[] a, int index, int d) {
-// if (a.length == index) {
-// return -1;
-// }
-// int b = index_of_occ(a, index + 1, d);
-// if(b == -1){
-// if (d == a[index]) {
-// return index;
-// } else {
-// return -1;
-// }
-// }
-// else{
-// return b;
-// }
